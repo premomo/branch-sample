be5303c4898d8c0b3c710ffe311bb03f1302fea7
premomo <premomo@gmail.com>
2025-11-11 13:28:53 +0900

    feat: Print fizzbuzz
    condition
    - i is multiple of 15.

--- fb.py
+++ fb.py
@@ -1,5 +1,9 @@
-for i in range(1, 15+1):
-    if i % 3 == 0:
+for j in range(1, 20+1):
+    # on branch main
+    # fizzbuzz
+    if j % 15 == 0:
+        print('fizzbuzz')
+    if j % 3 == 0:
         print('fizz')
     else:
-        print(i)
+        print(j)
